65a83e0547ecc1608d5c8913aff69197170dc535
Lukas Heller <dev@lukas-heller.de>
2022-09-26 16:54:52 +0200

    add route registration

--- app/Actions/MakeLaravelCrudAction.php
+++ app/Actions/MakeLaravelCrudAction.php
@@ -4,6 +4,7 @@ namespace App\Actions;
 
 use App\Commands\MakeCrudCommand;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Symfony\Component\Process\Process;
 
 class MakeLaravelCrudAction extends BaseMakeAction
@@ -19,7 +20,7 @@ class MakeLaravelCrudAction extends BaseMakeAction
     {
         $this->ensureBasicLaravelFilesExist();
 
-          $files = [
+        $files = [
             'controlller.php.stub' => [
                 'path' => 'Http/Controllers/',
                 'name' => ucfirst($this->name) . 'Controller.php',
@@ -35,22 +36,53 @@ class MakeLaravelCrudAction extends BaseMakeAction
         ];
 
         foreach ($files as $stub => $file) {
-            $targetPath = $this->basePath('admin/'.$file['path'] . $file['name']);
+            $targetPath = $this->basePath('admin/' . $file['path'] . $file['name']);
 
             if (File::exists($targetPath)) {
                 $this->command->error('File already exists at ' . $targetPath);
                 continue;
             }
 
-            File::ensureDirectoryExists($this->basePath('admin/'.$file['path']));
+            File::ensureDirectoryExists($this->basePath('admin/' . $file['path']));
 
             $stubPath = base_path('stubs/laravel/admin/' . $stub);
 
             $content = $this->replaceNameInContent(File::get($stubPath));
 
             File::put($targetPath, $content);
-
         }
+
+        $this->registerRoutes();
+
+    }
+
+       protected function registerRoutes()
+    {
+
+        $adminRoutes = File::get($this->basePath('/routes/admin.php'));
+
+        $controllerClassName = 'Admin\\Http\\Controllers\\' . ucfirst($this->name) . 'Controller';
+
+        // add controller use statement as the last import
+        if (! Str::contains($adminRoutes, $controllerClassName)) {
+            preg_match_all("/use .*?;/", $adminRoutes, $matches);
+            $lastMatch = $matches[0][count($matches[0]) - 1];
+            $newRouteImport = "use $controllerClassName;";
+            $adminRoutes = Str::replace($lastMatch, $lastMatch . "\n$newRouteImport", $adminRoutes);
+        } else {
+            $this->command->error('Controller Namespace already imported');
+        }
+
+        // Add route resource below last Route declaration withtin the api group
+        if (! Str::contains($adminRoutes, "Route::resource('{$this->name}s'")) {
+            $adminRoutes = preg_replace_callback('/\'api\',\s+?\],\s+?function\s+?\(\)\s+?\{((?:.*?|\n)*?)\s+?\}\)\;/', function ($matches) {
+                return Str::replaceLast(");\n", ");\n\n    //{$this->name}s\n    Route::resource('".$this->name."s', ".ucfirst($this->name)."Controller::class);\n", $matches[0]);
+            }, $adminRoutes);
+        } else {
+            $this->command->error("{$this->name}s routes already registered");
+        }
+
+        File::put($this->basePath('/routes/admin.php'), $adminRoutes);
     }
 
     public function ensureBasicLaravelFilesExist()
